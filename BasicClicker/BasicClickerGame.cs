5bc1f529f37315a960d44ceea8280e89392a201b
Stevie <xxlennygamerxx@gmail.com>
2020-12-12 16:36:07 -0800

    Reorganization and version display

--- BasicClicker/BasicClickerGame.cs
+++ BasicClicker/BasicClickerGame.cs
@@ -8,6 +8,8 @@ namespace BasicClicker
 {
     public class BasicClickerGame : Game
     {
+        public static BasicClickerGame Instance { get; private set; }
+
         /// <summary>
         /// GraphicsDeviceManager used by the entirety of BasicClicker.
         /// </summary>
@@ -18,15 +20,6 @@ namespace BasicClicker
         /// </summary>
         public SpriteBatch BCSpriteBatch { get; private set; }
 
-        public static bool GameIsIdle;
-
-        public static BasicClickerGame Instance { get; private set; }
-
-        /// <summary>
-        /// Logger used by BaseClicker.
-        /// </summary>
-        public static ILog Logger;
-
         public BasicClickerGame()
         {
             Instance = this;
@@ -35,50 +28,53 @@ namespace BasicClicker
             IsMouseVisible = true;
         }
 
+        // Called when the program gets focused
         protected override void OnActivated(object sender, EventArgs args)
         {
             Window.Title = "Basic Clicker";
 
-            GameIsIdle = false;
+            Main.GameIsIdle = false;
 
             base.OnActivated(sender, args);
         }
 
+        // Called when the program gets unfocused
         protected override void OnDeactivated(object sender, EventArgs args)
         {
+            // Indicate the program is on idle when unfocused
             Window.Title = "Basic Clicker - Idle";
 
-            GameIsIdle = true;
+            Main.GameIsIdle = true;
 
             base.OnDeactivated(sender, args);
         }
 
         protected override void Initialize()
         {
-            Logger = LogManager.GetLogger("BaseClicker");
+            Main.Logger.Info("Initializing");
 
-            Logger.Info("Initializing");
-
-            GameIsIdle = false;
+            Window.AllowUserResizing = true;
+            Window.AllowAltF4 = true;
 
             base.Initialize();
 
-            Logger.Info("Initialized");
+            Main.Logger.Info("Initialized");
         }
 
         protected override void LoadContent()
         {
-            Logger.Info("Loading content");
+            Main.Logger.Info("Loading content");
 
             BCSpriteBatch = new SpriteBatch(GraphicsDevice);
-
             AssetManager.LoadAssets();
 
-            Logger.Info("Content loaded");
+            Main.Logger.Info("Content loaded");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            Main.Update(gameTime);
+
             base.Update(gameTime);
         }
 
@@ -87,9 +83,7 @@ namespace BasicClicker
             GraphicsDevice.Clear(Color.White);
 
             BCSpriteBatch.Begin();
-
-            BCSpriteBatch.DrawString(AssetManager.Consola, "Test", new Vector2(0, 0), Color.Black);
-
+            Main.Draw(gameTime, BCSpriteBatch);
             BCSpriteBatch.End();
 
             base.Draw(gameTime);
---
+++ BasicClicker/Main.cs
@@ -0,0 +1,45 @@
+﻿using BasicClicker.Assets;
+using log4net;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace BasicClicker
+{
+    /// <summary>
+    /// Main class for most non-categorized fields and methods. <br />
+    /// Made for general-use stuff and to avoid cluttering <see cref="BasicClickerGame"/>.
+    /// </summary>
+    public static class Main
+    {
+        public static readonly Version BCVersion = new Version(0, 0, 1, 0);
+
+        public static bool GameIsIdle = false;
+        public static readonly string VersionText = $"BasicClicker v{BCVersion}";
+
+        /// <summary>
+        /// Logger used by BaseClicker.
+        /// </summary>
+        public static ILog Logger = LogManager.GetLogger("BaseClicker");
+
+        public static void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // Draw the version in the bottom-left-hand corner of the screen
+            DrawVersionText(spriteBatch);
+        }
+
+        public static void Update(GameTime gameTime)
+        {
+        }
+
+        public static void DrawVersionText(SpriteBatch spriteBatch)
+        {
+            GraphicsDevice graphicsDevice = BasicClickerGame.Instance.BCGraphicsDeviceManager.GraphicsDevice;
+            Vector2 versionTextSize = AssetManager.Consola.MeasureString(VersionText);
+            Vector2 versionPosFullscreen = new Vector2(0, graphicsDevice.DisplayMode.Height - versionTextSize.Y);
+            Vector2 versionPosWindowed = new Vector2(0, graphicsDevice.Viewport.Height - versionTextSize.Y);
+
+            spriteBatch.DrawString(AssetManager.Consola, VersionText, BasicClickerGame.Instance.BCGraphicsDeviceManager.IsFullScreen ? versionPosFullscreen : versionPosWindowed, Color.Black);
+        }
+    }
+}
