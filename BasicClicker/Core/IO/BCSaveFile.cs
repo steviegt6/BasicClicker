ba3deeb524d326ce0cd5488941c8e2e75022909b
Stevie <xxlennygamerxx@gmail.com>
2020-12-13 20:03:43 -0800

    Fix issue with path directories, setup basic data saving and loading for BC

---
+++ BasicClicker/Core/IO/BCSaveFile.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BasicClicker.Core.IO
+{
+    public class BCSaveFile : SaveFile
+    {
+        public override string FileName => "savedata";
+
+        public override void Save(FileStream stream, BinaryFormatter formatter)
+        {
+            formatter.Serialize(stream, 1);
+            formatter.Serialize(stream, "two");
+        }
+
+        public override void Load(FileStream stream, BinaryFormatter formatter)
+        {
+            int i = (int)formatter.Deserialize(stream);
+            string s = (string)formatter.Deserialize(stream);
+        }
+    }
+}
